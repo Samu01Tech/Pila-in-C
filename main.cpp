3b861a3653472f8057bb0d51048704f6d9616952
Samuele Mazzei <samuele.mazzei@studenti.unitn.it>
2021-03-08 08:07:55 +0000

    debug

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cassert>
+#define DEBUG
 using namespace std;
 
 struct Pila {
@@ -20,20 +21,25 @@ int main() {
 }
 
 Pila* crea(int initialSize){
+  cout << "Eseguendo crea..." << endl;
   Pila *s = new Pila;
   s->size = initialSize;
   s->defaultGrowthSize = initialSize;
   s->marker = 0;
   s->contenuto = new int[initialSize];
   return s;
+  cout << "Fatto" << endl;
 }
 
 void distruggi(Pila *s){
+  cout << "Eseguendo distruggi..." << endl;
   delete [] (s->contenuto);
   delete s;
+  cout << "Fatto" << endl;
 }
 
 void cresci(Pila *s, int increment){
+  cout << "Eseguendo cresci..." << endl;
   s->size += increment;
   int* temp = new int[s->size];
   for(int k=0; k < s->marker; k++){
@@ -41,6 +47,7 @@ void cresci(Pila *s, int increment){
   }
   delete [] (s->contenuto);
   s->contenuto = temp;
+  cout << "Fatto" << endl;
 }
 
 void inserisci(Pila *s, int k){
@@ -50,10 +57,17 @@ void inserisci(Pila *s, int k){
   }
   s->contenuto[s->marker] = k;
   s->marker++;
+  cout << "Fatto" << endl;
 }
 
 int estrai(Pila *s){
   cout << "Eseguendo estrai..." << endl;
-  assert(s->marker>0);
+  
+  #ifdef DEBBUG
+    assert(s->marker>0);
+    //serve in fase di debug per verificare il corretto funzionamento del programma
+  #endif
+
   return s->contenuto[--(s->marker)];
+  cout << "Fatto" << endl;
 }
